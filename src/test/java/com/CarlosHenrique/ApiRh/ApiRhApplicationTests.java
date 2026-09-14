657439229cb3bf1c3752905cfe7bbb58ffa395f9
Carlos Henrique <ccarlos@MacBook-Air-de-Carlos.local>
2026-09-13 21:11:54 -0300

    enviando projeto para o git

---
+++ src/test/java/com/CarlosHenrique/ApiRh/ApiRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CarlosHenrique.ApiRh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
