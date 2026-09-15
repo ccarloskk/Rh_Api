82b94588314610f45d0c4953d23a6d1015b66ec8
Carlos Henrique <ccarlos@MacBook-Air-de-Carlos.local>
2026-09-14 21:04:13 -0300

    Criacao de Model Departamento completo

---
+++ src/main/java/com/CarlosHenrique/ApiRh/Model/Departamento.java
@@ -0,0 +1,18 @@
+package com.CarlosHenrique.ApiRh.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+
+}
